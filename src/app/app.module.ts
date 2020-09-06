afb6dd49b77579817e9c7e89964aeff43ce702f9
Lahiru Jayamanna <lsampath999@gmail.com>
2020-09-06 23:19:54 +0800

    refactor styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { appRoutingModule } from './app.routing';
 import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home';
+import { WelcomeComponent } from './welcome';
 import { LoginComponent } from './login';
 import { RegisterComponent } from './register';
 import { AlertComponent } from './_components';
@@ -24,6 +25,7 @@ import { AlertComponent } from './_components';
     declarations: [
         AppComponent,
         HomeComponent,
+        WelcomeComponent,
         LoginComponent,
         RegisterComponent,
         AlertComponent
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,12 +2,13 @@
 
 import { HomeComponent } from './home';
 import { LoginComponent } from './login';
+import { WelcomeComponent } from './welcome';
 import { RegisterComponent } from './register';
 import { AuthGuard } from './_helpers';
 
 const routes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-    { path: 'login', component: LoginComponent },
+    { path: 'login', component: WelcomeComponent },
     { path: 'register', component: RegisterComponent },
     { path: '**', redirectTo: '' }
 ];
